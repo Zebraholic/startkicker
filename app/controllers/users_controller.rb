4d41c41cae9f9189c7deb39d02d9288c48bd2aef
Zebraholic <rctavares@rogers.com>
2014-12-03 16:00:39 -0500

    Added sign up view/authentication and generated mailer for sorcery.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login(@user)
+      redirect_to projects_url
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -19,4 +27,10 @@ class UsersController < ApplicationController
 
   def show
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
+
